dd82603655b8c8bb59b0a1062f295f6f466f9972
ARYA <aryapn200605@gmail.com>
2023-02-13 15:37:21 +0700

    food 2.0

---
+++ app/src/main/java/com/example/relativelayout_arya_12rpl/model/CategoriesModel.kt
@@ -0,0 +1,11 @@
+package com.example.relativelayout_arya_12rpl.model
+
+class CategoriesModel: java.io.Serializable {
+    @JvmField
+    var strCategory: String? = null
+
+    @JvmField
+    var strCategoryThumb: String? = null
+
+    var strCategoryDescription: String? = null
+}
---
+++ app/src/main/java/com/example/relativelayout_arya_12rpl/networking/Api.kt
@@ -0,0 +1,5 @@
+package com.example.relativelayout_arya_12rpl.networking
+
+object Api {
+    var categories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+}
