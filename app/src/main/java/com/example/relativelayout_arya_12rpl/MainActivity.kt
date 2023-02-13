dfbc1b3775a6ba4435cd9922eae15b3e4c2d95a5
ARYA <aryapn200605@gmail.com>
2023-02-13 10:11:10 +0700

    Relative Layout

---
+++ app/src/main/java/com/example/relativelayout_arya_12rpl/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.relativelayout_arya_12rpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSpinnerDay()
+        setSpinnerTime()
+    }
+
+    private fun setSpinnerDay() {
+        val spinnerDay: Spinner = findViewById(R.id.spin_date)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.day,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item
+        ).also {
+            it.setDropDownViewResource(
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item
+            )
+            spinnerDay.adapter = it
+        }
+    }
+
+    private fun setSpinnerTime() {
+        val spinnerTime: Spinner = findViewById(R.id.spin_time)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.time,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item
+        ).also {
+            it.setDropDownViewResource(
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item
+            )
+            spinnerTime.adapter = it
+        }
+    }
+}
